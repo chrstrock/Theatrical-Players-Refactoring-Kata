00ad37a01944ee21f3d69807a27f2e8e1744d4ea
David Ferrell <chrstrock@gmail.com>
2026-05-29 16:19:09 -0400

    extract method for getVolumeCredits.

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -14,11 +14,7 @@ public class StatementPrinter {
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (var perf : invoice.performances) {
-
-            // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy".equals(playFor(plays, perf).type)) volumeCredits += Math.floor(perf.audience / 5);
+            volumeCredits = getVolumeCredits(plays, perf);
 
             // print line for this order
             result += String.format("  %s: %s (%s seats)%n", playFor(plays, perf).name, frmt.format(getThisAmount(perf, plays) / 100), perf.audience);
@@ -26,6 +22,14 @@ public class StatementPrinter {
         }
         result += String.format("Amount owed is %s%n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits%n", volumeCredits);
+        return result;
+    }
+
+    private static int getVolumeCredits(Map<String, Play> plays, Performance perf) {
+        int result;
+        result = Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy".equals(playFor(plays, perf).type)) result += Math.floor(perf.audience / 5);
         return result;
     }
 
