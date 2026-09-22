f4dab7fb1c0d660b999e4fa852698bd7e927311f
David Ferrell <chrstrock@gmail.com>
2026-09-22 13:52:55 -0400

    r replace thisAmount with inline variable.

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -17,16 +17,14 @@ public class StatementPrinter {
 
         for (var perf : invoice.performances) {
 
-            var thisAmount = getThisAmount(perf);
-
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
             if ("comedy".equals(playFor(perf).type)) volumeCredits += Math.floor(perf.audience / 5);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)%n", playFor(perf).name, frmt.format(thisAmount / 100), perf.audience);
-            totalAmount += thisAmount;
+            result += String.format("  %s: %s (%s seats)%n", playFor(perf).name, frmt.format(getThisAmount(perf) / 100), perf.audience);
+            totalAmount += getThisAmount(perf);
         }
         result += String.format("Amount owed is %s%n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits%n", volumeCredits);
