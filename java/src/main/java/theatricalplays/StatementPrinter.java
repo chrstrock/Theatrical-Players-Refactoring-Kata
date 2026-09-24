6e9820208e4ba23f3a65339bf35e1675c5cb7fa3
David Ferrell <chrstrock@gmail.com>
2026-09-24 08:39:27 -0400

    Casting fix on volume credits due to double, int confusion.

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -38,7 +38,8 @@ public class StatementPrinter {
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
-            if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+            if ("comedy".equals(play.type))
+                volumeCredits = (int) (volumeCredits + Math.floor((double) perf.audience / 5));
 
             // print line for this order
             result += String.format("  %s: %s (%s seats)%n", play.name, frmt.format(thisAmount / 100), perf.audience);
