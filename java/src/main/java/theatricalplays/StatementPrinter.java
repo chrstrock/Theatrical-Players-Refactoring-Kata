e89a1d5f20f2263ca09196e359f17e7227029505
David Ferrell <chrstrock@gmail.com>
2026-09-24 08:56:01 -0400

    Rename perf to performance in getThisAmount

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -32,22 +32,22 @@ public class StatementPrinter {
         return result.toString();
     }
 
-    private static int getThisAmount(Performance perf, Play play) {
+    private static int getThisAmount(Performance performance, Play play) {
         var result = 0;
 
         switch (play.type) {
             case "tragedy":
                 result = 40000;
-                if (perf.audience > 30) {
-                    result += 1000 * (perf.audience - 30);
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if (perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20);
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20);
                 }
-                result += 300 * perf.audience;
+                result += 300 * performance.audience;
                 break;
             default:
                 throw new Error("unknown type: %s".formatted(play.type));
